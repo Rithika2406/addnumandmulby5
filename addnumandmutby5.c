1829b50dad5afcbb69f1b7e21f934b872ad974ad
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-08 14:06:25 +0530

    Add files via upload

---
+++ addnumandmutby5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int t,X;
+    int i = 1;
+    scanf("%d", &t );
+    while (i <= t){
+        scanf("%d", &X);
+        int Y=X+X;
+        printf("%d\n",5*Y);
+        i=i+1;
+
+
+      
+    }
+    return 0;
+}
+ 
